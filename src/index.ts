c0eac703b13fb08b880cf89fabded95a91713a39
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-05-14 21:50:52 +0700

    Refactor: Try method PUT for edit car by Id

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import http from 'http';
 import express from 'express';
 import carsRouter from './routes/car.routes';
+import bodyParser from 'body-parser';
 
 
 const port = 3000;
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).json({
--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -1,37 +1,60 @@
 import { Router, Request, Response } from "express";
-import { cars } from '../__data_mocks__/cars';
+import { cars } from "../__data_mocks__/cars";
 import filterCars from "../utils/filter";
 import { connect } from "http2";
 
 const router = Router();
 
 // Get
-router.get('/', (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-  cars
+    cars,
   });
 });
 
 // Get specific car by id
-router.get('/:id', (req: Request, res: Response) => {
-  const getId:number = Number(req.params.id);
+router.get("/:id", (req: Request, res: Response) => {
+  const getId: number = Number(req.params.id);
   console.log(getId);
-  
+
   // const carById = cars.find((car) => car.id === getId);
   // res.status(200).json({
   //   carById
   // });
   const carById = filterCars(cars, getId);
   res.status(200).json({
-    cars: carById
+    cars: carById,
   });
 });
 
-//Update /EdIT
-router.put('/:id', (req: Request, res: Response) => {
-  const getId:number = Number(req.params.id);
-  const { name, price, startRent } = req.body;
+//Update /Edit
+router.put("/:id", (req: Request, res: Response) => {
+  const getId: number = Number(req.params.id);
+  const { name, price, startRent} = req.body;
   const carById = filterCars(cars, getId);
-})
+
+  const updatedCarByID = {
+    ...carById,
+    id: getId,
+    name: "Toyota",
+    price: 200000,
+    startRent: "05/04/2022",
+    createdAt: "02/04/2022",
+    updatedAt: "02/04/2022",
+  };
+  const filterUpdatedCar = cars.filter((car) => car.id !== getId);
+  // new data cars
+  filterUpdatedCar.push(updatedCarByID);
+  filterUpdatedCar;
+
+  console.log({filterUpdatedCar});
+
+  res.status(204).json({
+    status: "success",
+    message: "Car updated successfully",
+    cars: filterUpdatedCar,
+  });
+  // console.log({carById});
+});
 
 export default router;
